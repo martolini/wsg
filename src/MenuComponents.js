89c4b97b5a0747d33e3ab10711bbbfc2aa84b303
Martin Røed <martinroed@Martins-MacBook-Pro.local>
2018-04-25 00:36:16 +0200

    Can now press enter on select field, fixed routing issue.

--- src/MenuComponents.js
+++ src/MenuComponents.js
@@ -1,54 +1,58 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 class OptionComponent extends Component {
-
-  handleMouseDown = (event) => {
+  handleMouseDown = event => {
     event.preventDefault();
     event.stopPropagation();
     this.props.onSelect(this.props.option, event);
-  }
+  };
 
-  handleMouseEnter = (event) => {
+  handleMouseEnter = event => {
     this.props.onFocus(this.props.option, event);
-  }
+  };
 
-  handleMouseMove = (event) => {
+  handleMouseMove = event => {
     if (this.props.isFocused) return;
     this.props.onFocus(this.props.option, event);
-  }
+  };
 
   render() {
     const { option, className } = this.props;
     return (
-        <Link to={{
-            pathname: `/${option.value}`,
-            state: { fromDashboard: true }
-          }}
-              className={className}>
-          <div>
-          {option.posterURL !== 'N/A' &&
-            <img alt="" src={option.posterURL}/>
-          }
-          </div>
-          <span>{option.title}</span>
-        </Link>
-      )
+      <Link
+        to={{
+          pathname: `/${option.value}`,
+          state: { fromDashboard: true }
+        }}
+        onClick={console.log}
+        className={className}
+      >
+        <div>
+          {option.posterURL !== "N/A" && <img alt="" src={option.posterURL} />}
+        </div>
+        <span>{option.title}</span>
+      </Link>
+    );
   }
 }
 
-const ValueComponent = ({value}) => (
-	<div className="value-component Select-value">
+const ValueComponent = ({ value }) => (
+  <div className="value-component Select-value">
     <div>
-      <img alt="" src={value.posterURL}/>
+      <img alt="" src={value.posterURL} />
     </div>
     <span>{value.title}</span>
-    <div  style={{float: 'right', paddingRight: 15}}>
-      <i className='fa fa-star' style={{color: '#f7c61f'}} aria-hidden='true' />
-      <span className=''> {value.rating}</span>
+    <div style={{ float: "right", paddingRight: 15 }}>
+      <i
+        className="fa fa-star"
+        style={{ color: "#f7c61f" }}
+        aria-hidden="true"
+      />
+      <span className=""> {value.rating}</span>
     </div>
-	</div>
-)
+  </div>
+);
 
-export {OptionComponent, ValueComponent}
+export { OptionComponent, ValueComponent };
--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import App from './App';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import React from "react";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const Routes = () => 
-	(<Router>
-		<div>
-			<Route exact path="/" component={App} />
-			<Route path="/:showid" component={App} />
-			<Route path="/:showid/:episodeid" component={App} />
-		</div>
-	</Router>)
+const Routes = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route exact path="/:showid" component={App} />
+      <Route exact path="/:showid/:episodeid" component={App} />
+    </div>
+  </Router>
+);
 
 export default Routes;
